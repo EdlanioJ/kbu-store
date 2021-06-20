fbd419fb42343b813703bdc4f4e580f60d845b54
Edlâneo Júlio <edlanioj@gmail.com>
2021-06-20 20:03:09 +0000

    recreate  factories

--- app/factory/category.go
+++ app/factory/category.go
@@ -3,43 +3,13 @@ package factory
 import (
 	"time"
 
-	"github.com/EdlanioJ/kbu-store/category/handler"
 	gormRepo "github.com/EdlanioJ/kbu-store/category/repository/gorm"
 	"github.com/EdlanioJ/kbu-store/category/usecase"
 	"gorm.io/gorm"
 )
 
-func CreateCategoryHandler(db *gorm.DB, contextTimeout time.Duration) *handler.CreateHandler {
-	createRepo := gormRepo.NewGormCreateCategory(db)
+func CategoryUsecase(db *gorm.DB, contextTimeout time.Duration) *usecase.CategoryUsecase {
+	categoryRepo := gormRepo.NewCategoryRepository(db)
 
-	createUsecase := usecase.NewCreateCategory(createRepo, contextTimeout)
-	return handler.NewCreateHandler(createUsecase)
-}
-
-func FetchCategoryHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchHandler {
-	fetchRepo := gormRepo.NewGormFetchCategory(db)
-
-	fetchUsecase := usecase.NewFetchCategory(fetchRepo, contextTimeout)
-	return handler.NewFetchHandler(fetchUsecase)
-}
-
-func FetchCategoryByStatusHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchByStatusHandler {
-	fetchRepo := gormRepo.NewGormFetchCategoryByStatus(db)
-
-	fetchByStatusUsecase := usecase.NewFetchCategoryByStatus(fetchRepo, contextTimeout)
-	return handler.NewFetchByStatusHandler(fetchByStatusUsecase)
-}
-
-func GetCategoryByIDHandler(db *gorm.DB, contextTimeout time.Duration) *handler.GetByIDHandler {
-	getRepo := gormRepo.NewGormGetCategoryByID(db)
-
-	getUsecase := usecase.NewGetCategoryByID(getRepo, contextTimeout)
-	return handler.NewGetByIDHandler(getUsecase)
-}
-
-func GetCategoryByStatusHandler(db *gorm.DB, contextTimeout time.Duration) *handler.GetByStatusHandler {
-	getRepo := gormRepo.NewGormGetCategoryByStatus(db)
-
-	getUsecase := usecase.NewGetCategoryByStatus(getRepo, contextTimeout)
-	return handler.NewGetByStatusHandler(getUsecase)
+	return usecase.NewCategoryUsecase(categoryRepo, contextTimeout)
 }
--- app/factory/store.go
+++ app/factory/store.go
@@ -5,121 +5,21 @@ import (
 
 	gormAccountRepo "github.com/EdlanioJ/kbu-store/account/repository/gorm"
 	gormCategoryRepo "github.com/EdlanioJ/kbu-store/category/repository/gorm"
-	"github.com/EdlanioJ/kbu-store/store/handler"
 	gormStoreRepo "github.com/EdlanioJ/kbu-store/store/repository/gorm"
 	"github.com/EdlanioJ/kbu-store/store/usecase"
 	"gorm.io/gorm"
 )
 
-func ActiveStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.ActiveHandler {
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByID(db)
-	updateStore := gormStoreRepo.NewGormUpdateStore(db)
+func StoreUsecase(db *gorm.DB, contextTimeout time.Duration) *usecase.StoreUsecase {
+	storeRepo := gormStoreRepo.NewStoreRepository(db)
+	accountRepo := gormAccountRepo.NewAccountRepository(db)
+	categoryRepo := gormCategoryRepo.NewCategoryRepository(db)
 
-	activeUsecase := usecase.NewActivateStore(getStoreRepo, updateStore, contextTimeout)
-	return handler.NewActiveHandler(activeUsecase)
+	return usecase.NewStoreUsecase(storeRepo, accountRepo, categoryRepo, contextTimeout)
 }
 
-func BlockStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.BlockHandler {
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByID(db)
-	updateStore := gormStoreRepo.NewGormUpdateStore(db)
+func TagUsecase(db *gorm.DB, contextTimeout time.Duration) *usecase.TagUsecase {
+	tagRepo := gormStoreRepo.NewTagsRepository(db)
 
-	blockUsecase := usecase.NewBlockStore(getStoreRepo, updateStore, contextTimeout)
-	return handler.NewBlockHandler(blockUsecase)
-}
-
-func DisableStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.DisableHandler {
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByID(db)
-	updateStore := gormStoreRepo.NewGormUpdateStore(db)
-
-	disableUsecase := usecase.NewDisableStore(getStoreRepo, updateStore, contextTimeout)
-	return handler.NewDisableHandler(disableUsecase)
-}
-
-func CreateStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.CreateHandler {
-	createStoreRepo := gormStoreRepo.NewGormCreateStore(db)
-	createAccountRepo := gormAccountRepo.NewGormCreateAccount(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	createUsecase := usecase.NewCreateStore(createStoreRepo, createAccountRepo, getCategoryRepo, contextTimeout)
-	return handler.NewCreateHandler(createUsecase)
-}
-
-func DeleteStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.DeleteHandler {
-	deleteStoreRepo := gormStoreRepo.NewGormDeleteStore(db)
-	deleteAccountRepo := gormAccountRepo.NewGormDeleteAccount(db)
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByID(db)
-
-	deleteUsecase := usecase.NewDeleteStore(deleteStoreRepo, deleteAccountRepo, getStoreRepo, contextTimeout)
-	return handler.NewDeleteHandler(deleteUsecase)
-}
-
-func FetchByLocationAndStatusHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchByLocationAndStatusHandler {
-	fetchByLocationRepo := gormStoreRepo.NewGormFetchStoreByCloseLocation(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	fetchByLocation := usecase.NewFetchStoreByCloseLocation(fetchByLocationRepo, getCategoryRepo, contextTimeout)
-	return handler.NewFetchByLocationAndStatusHandler(fetchByLocation)
-}
-func FetchStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchHandler {
-	fetchStoreRepo := gormStoreRepo.NewGormFetchStore(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	fetchUsecase := usecase.NewFetchStore(fetchStoreRepo, getCategoryRepo, contextTimeout)
-	return handler.NewFetchHandler(fetchUsecase)
-}
-
-func FetchStoreByOwnerHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchByOwnerHandler {
-	fetchByOwnerRepo := gormStoreRepo.NewGormFetchStoreByOwner(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	fetchByOwnerUsecase := usecase.NewFetchStoreByOwner(fetchByOwnerRepo, getCategoryRepo, contextTimeout)
-	return handler.NewFetchByOwnerHandler(fetchByOwnerUsecase)
-}
-
-func FetchStoreByStatusHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchByStatusHandler {
-	fetchByStatusRepo := gormStoreRepo.NewGormFetchStoreByStauts(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	fetchByStatusUsecase := usecase.NewFetchStoreByStatus(fetchByStatusRepo, getCategoryRepo, contextTimeout)
-	return handler.NewFetchByStatusHandler(fetchByStatusUsecase)
-}
-
-func FetchStoreByTagsHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchByTagsHandler {
-	fetchByTagsRepo := gormStoreRepo.NewGormFetchStoreByTags(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	fetchByTagsUsecase := usecase.NewFetchStoreByTabs(fetchByTagsRepo, getCategoryRepo, contextTimeout)
-	return handler.NewFetchByTagsHandler(fetchByTagsUsecase)
-}
-
-func FetchStoreByTypeHandler(db *gorm.DB, contextTimeout time.Duration) *handler.FetchByTypeHandler {
-	fetchByTypeRepo := gormStoreRepo.NewGormFetchStoreByCategory(db)
-	getCategoryRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	fetchByTypeUsecase := usecase.NewFetchStoreByCategory(fetchByTypeRepo, getCategoryRepo, contextTimeout)
-	return handler.NewFetchByTypeHandler(fetchByTypeUsecase)
-}
-
-func GetStoreByIDHandler(db *gorm.DB, contextTimeout time.Duration) *handler.GetByIDHandler {
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByID(db)
-	getStorTypeRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	getbyIDUsecase := usecase.NewGetStoreByID(getStoreRepo, getStorTypeRepo, contextTimeout)
-	return handler.NewGetByIDHandler(getbyIDUsecase)
-}
-
-func GetStoreByOwnerHandler(db *gorm.DB, contextTimeout time.Duration) *handler.GetByOwnerHandler {
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByOwner(db)
-	getStorTypeRepo := gormCategoryRepo.NewGormGetCategoryByID(db)
-
-	getbyIDUsecase := usecase.NewGetStoreByOwner(getStoreRepo, getStorTypeRepo, contextTimeout)
-	return handler.NewGetByOwnerHandler(getbyIDUsecase)
-}
-
-func UpdateStoreHandler(db *gorm.DB, contextTimeout time.Duration) *handler.UpdateHandler {
-	updateStoreRepo := gormStoreRepo.NewGormUpdateStore(db)
-	getStoreRepo := gormStoreRepo.NewGormGetStoreByID(db)
-
-	updateUsecase := usecase.NewUpdateStore(getStoreRepo, updateStoreRepo, contextTimeout)
-	return handler.NewUpdateHandler(updateUsecase)
+	return usecase.NewtagUsecase(tagRepo, contextTimeout)
 }
