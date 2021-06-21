9c692b93a21b9d46cc66dab30202b4580b9a2314
Edlâneo Júlio <edlanioj@gmail.com>
2021-06-21 19:27:23 +0000

    remove logger and requestId middleware

--- app/main.go
+++ app/main.go
@@ -14,8 +14,6 @@ import (
 	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/gofiber/helmet/v2"
 )
 
@@ -41,8 +39,6 @@ func main() {
 
 	app.Use(cors.New())
 	app.Use(helmet.New())
-	app.Use(requestid.New())
-	app.Use(logger.New())
 
 	v1 := app.Group("/api/v1")
 	v1.Get("/docs/*", swagger.Handler)
