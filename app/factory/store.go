ddcbc0f870d6bdc845ed2c0955f63e8e5c450bba
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-01 21:53:05 +0000

    update tags usecase import name

--- app/factory/store.go
+++ app/factory/store.go
@@ -5,18 +5,17 @@ import (
 
 	gormAccountRepo "github.com/EdlanioJ/kbu-store/account/repository/gorm"
 	gormCategoryRepo "github.com/EdlanioJ/kbu-store/category/repository/gorm"
-	dataUsecase "github.com/EdlanioJ/kbu-store/data/usecase"
+	"github.com/EdlanioJ/kbu-store/data/usecase"
 	gormStoreRepo "github.com/EdlanioJ/kbu-store/store/repository/gorm"
-	"github.com/EdlanioJ/kbu-store/store/usecase"
 	"gorm.io/gorm"
 )
 
-func StoreUsecase(db *gorm.DB, contextTimeout time.Duration) *dataUsecase.StoreUsecase {
+func StoreUsecase(db *gorm.DB, contextTimeout time.Duration) *usecase.StoreUsecase {
 	storeRepo := gormStoreRepo.NewStoreRepository(db)
 	accountRepo := gormAccountRepo.NewAccountRepository(db)
 	categoryRepo := gormCategoryRepo.NewCategoryRepository(db)
 
-	return dataUsecase.NewStoreUsecase(storeRepo, accountRepo, categoryRepo, contextTimeout)
+	return usecase.NewStoreUsecase(storeRepo, accountRepo, categoryRepo, contextTimeout)
 }
 
 func TagUsecase(db *gorm.DB, contextTimeout time.Duration) *usecase.TagUsecase {
