919d3787c6cfc4a95342c34b08786da0bf1db129
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-02 14:02:34 +0000

    create  pg conection

---
+++ infra/db/pg.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func SqlConnection(dns string) *sql.DB {
+	db, err := sql.Open("postgres", dns)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
