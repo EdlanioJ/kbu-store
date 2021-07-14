41014e38adef31952b564444ee87879c5a326bb3
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-14 10:11:26 +0000

    add new err verification on getStatusCode func

--- application/http/handler/errors.go
+++ application/http/handler/errors.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/EdlanioJ/kbu-store/domain"
+	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -14,16 +15,21 @@ type ErrorResponse struct {
 func getStatusCode(err error) int {
 	logrus.Error(err)
 
+	if _, ok := err.(govalidator.Errors); ok {
+		return fiber.StatusBadRequest
+	}
+
 	switch err {
 	case domain.ErrNotFound,
 		gorm.ErrRecordNotFound:
 		return fiber.StatusNotFound
+	case domain.ErrBadParam:
+		return fiber.StatusBadRequest
 	case domain.ErrActived,
 		domain.ErrBlocked,
-		domain.ErrBadParam,
 		domain.ErrInactived,
 		domain.ErrIsPending:
-		return fiber.StatusBadRequest
+		return fiber.StatusConflict
 	}
 	return fiber.StatusInternalServerError
 }
