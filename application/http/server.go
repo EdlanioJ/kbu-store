449b4a7aa4140b4cc03ff7b35e78b4cbae52226b
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-18 12:00:14 +0000

    add stores routes no server start func

--- application/http/server.go
+++ application/http/server.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/EdlanioJ/kbu-store/application/factory"
 	_ "github.com/EdlanioJ/kbu-store/application/http/docs"
-	routes "github.com/EdlanioJ/kbu-store/application/http/handler"
+	"github.com/EdlanioJ/kbu-store/application/http/handler"
 	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -26,7 +26,6 @@ import (
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @BasePath /api/v1
 func StartServer(database *gorm.DB, tc time.Duration, port int) {
-
 	app := fiber.New()
 
 	app.Use(cors.New())
@@ -35,13 +34,24 @@ func StartServer(database *gorm.DB, tc time.Duration, port int) {
 	v1 := app.Group("/api/v1")
 
 	v1.Get("/docs/*", swagger.Handler)
-	su := factory.StoreUsecase(database, tc)
+	storeUsecase := factory.StoreUsecase(database, tc)
 	tu := factory.TagUsecase(database, tc)
 	cu := factory.CategoryUsecase(database, tc)
 
-	routes.NewStoreRoute(v1, su)
-	routes.NewTagRoutes(v1, tu)
-	routes.NewCategoryRoutes(v1, cu)
+	storeRoutes := v1.Group("/stores")
+	storeHandler := handler.NewStoreHandler(storeUsecase)
+
+	storeRoutes.Post("/", storeHandler.Store)
+	storeRoutes.Get("/", storeHandler.Index)
+	storeRoutes.Get("/:id", storeHandler.Get)
+	storeRoutes.Patch("/:id", storeHandler.Update)
+	storeRoutes.Patch("/:id/activate", storeHandler.Activate)
+	storeRoutes.Patch("/:id/block", storeHandler.Block)
+	storeRoutes.Patch("/:id/disable", storeHandler.Disable)
+	storeRoutes.Delete("/:id", storeHandler.Delete)
+
+	handler.NewTagRoutes(v1, tu)
+	handler.NewCategoryRoutes(v1, cu)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
 }
