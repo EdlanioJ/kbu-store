0c49a0de5ceba6d0fcaffb51ec56d702191008de
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-20 09:38:56 +0000

    Add kafka producer in store factory

--- application/factory/store.go
+++ application/factory/store.go
@@ -4,14 +4,16 @@ import (
 	"time"
 
 	gormRepo "github.com/EdlanioJ/kbu-store/infra/db/repository/gorm"
+	"github.com/EdlanioJ/kbu-store/infra/kafka"
 	"github.com/EdlanioJ/kbu-store/usecase"
 	"gorm.io/gorm"
 )
 
-func StoreUsecase(db *gorm.DB, contextTimeout time.Duration) *usecase.StoreUsecase {
+func StoreUsecase(db *gorm.DB, timeout time.Duration, brokers []string) *usecase.StoreUsecase {
 	storeRepo := gormRepo.NewStoreRepository(db)
 	accountRepo := gormRepo.NewAccountRepository(db)
 	categoryRepo := gormRepo.NewCategoryRepository(db)
+	kafkaProducer := kafka.NewKafkaProducer(brokers)
 
-	return usecase.NewStoreUsecase(storeRepo, accountRepo, categoryRepo, contextTimeout)
+	return usecase.NewStoreUsecase(storeRepo, accountRepo, categoryRepo, kafkaProducer, timeout)
 }
