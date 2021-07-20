0f717a8a07aa5ac1796d8994e27da4f81a486f59
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-20 09:37:33 +0000

    Refractor http server

--- application/cmd/http.go
+++ application/cmd/http.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/EdlanioJ/kbu-store/application/config"
+	"github.com/EdlanioJ/kbu-store/application/factory"
 	"github.com/EdlanioJ/kbu-store/application/http"
 	"github.com/EdlanioJ/kbu-store/infra/db"
 	"github.com/spf13/cobra"
@@ -27,8 +28,15 @@ var httpCmd = &cobra.Command{
 			database = db.GORMConnection(config.Dns, config.Env)
 		}
 
-		tc := time.Duration(config.TimeoutContext) * time.Second
-		http.StartServer(database, tc, config.Port)
+		tc := time.Duration(config.Timeout) * time.Second
+
+		httpServer := http.NewHttpServer()
+
+		httpServer.StoreUsecase = factory.StoreUsecase(database, tc, config.KafkaBrokers)
+		httpServer.CategoryUsecase = factory.CategoryUsecase(database, tc)
+		httpServer.Port = config.Port
+
+		httpServer.Serve()
 	},
 }
 
--- application/http/server.go
+++ application/http/server.go
@@ -3,20 +3,28 @@ package http
 import (
 	"fmt"
 	"log"
-	"time"
 
-	"github.com/EdlanioJ/kbu-store/application/factory"
 	_ "github.com/EdlanioJ/kbu-store/application/http/docs"
 	"github.com/EdlanioJ/kbu-store/application/http/handler"
 	"github.com/EdlanioJ/kbu-store/application/http/middleware"
+	"github.com/EdlanioJ/kbu-store/domain"
 	fiberprometheus "github.com/ansrivas/fiberprometheus/v2"
 	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/gofiber/helmet/v2"
-	"gorm.io/gorm"
 )
+
+type httpServer struct {
+	Port            int
+	StoreUsecase    domain.StoreUsecase
+	CategoryUsecase domain.CategoryUsecase
+}
+
+func NewHttpServer() *httpServer {
+	return &httpServer{}
+}
 
 // @title KBU Store API
 // @version 2.0.0
@@ -27,7 +35,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @BasePath /api/v1
-func StartServer(database *gorm.DB, tc time.Duration, port int, kafkaBrokes []string) {
+func (s *httpServer) Serve() {
 	app := fiber.New(fiber.Config{
 		ErrorHandler: middleware.ErrorHandler(),
 	})
@@ -43,10 +51,8 @@ func StartServer(database *gorm.DB, tc time.Duration, port int, kafkaBrokes []st
 	v1 := app.Group("/api/v1")
 
 	v1.Get("/docs/*", swagger.Handler)
-	cu := factory.CategoryUsecase(database, tc)
 
-	storeUsecase := factory.StoreUsecase(database, tc, kafkaBrokes)
-	storeHandler := handler.NewStoreHandler(storeUsecase)
+	storeHandler := handler.NewStoreHandler(s.StoreUsecase)
 	storeRoutes := v1.Group("/stores")
 
 	storeRoutes.Post("/", storeHandler.Store)
@@ -58,9 +64,9 @@ func StartServer(database *gorm.DB, tc time.Duration, port int, kafkaBrokes []st
 	storeRoutes.Patch("/:id/disable", storeHandler.Disable)
 	storeRoutes.Delete("/:id", storeHandler.Delete)
 
-	handler.NewCategoryRoutes(v1, cu)
+	handler.NewCategoryRoutes(v1, s.CategoryUsecase)
 
 	app.Use(middleware.NotFound())
 
-	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", s.Port)))
 }
