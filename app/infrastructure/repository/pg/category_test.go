15887d7b49db6e32eb299135a1625ff4fffb2d80
Edlâneo Júlio <edlanioj@gmail.com>
2021-07-28 20:25:07 +0000

    Refactor sql category repository tests

--- app/infrastructure/repository/pg/category_test.go
+++ app/infrastructure/repository/pg/category_test.go
@@ -9,197 +9,185 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/EdlanioJ/kbu-store/app/domain"
 	"github.com/EdlanioJ/kbu-store/app/infrastructure/repository/pg"
+	"github.com/EdlanioJ/kbu-store/app/utils/sample"
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-func getCategory() *domain.Category {
-	id := uuid.NewV4().String()
-	name := "Type 001"
-	category, _ := domain.NewCategory(id, name, domain.CategoryStatusInactive)
-
-	return category
-}
-
 func Test_CategoryRepo_Store(t *testing.T) {
-	c := getCategory()
+	c := sample.NewCategory()
 	testCases := []struct {
-		name          string
-		arg           *domain.Category
-		builtSts      func(mock sqlmock.Sqlmock)
-		checkResponse func(t *testing.T, err error)
+		name        string
+		arg         *domain.Category
+		expectedErr bool
+		prepare     func(mock sqlmock.Sqlmock)
 	}{
 		{
-			name: "fail on exec query",
-			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "failur_exec_query_returns_error",
+			arg:         c,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `INSERT INTO categories (id,created_at,updated_at,name,status) VALUES ($1,$2,$3,$4,$5)`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.ID, c.CreatedAt, c.UpdatedAt, c.Name, c.Status).WillReturnError(errors.New("unexpected error"))
 			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.Error(t, err)
-			},
 		},
 		{
-			name: "fail on get affected row",
-			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "fail_get_affected_row_returns_error",
+			arg:         c,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `INSERT INTO categories (id,created_at,updated_at,name,status) VALUES ($1,$2,$3,$4,$5)`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.ID, c.CreatedAt, c.UpdatedAt, c.Name, c.Status).WillReturnResult(sqlmock.NewErrorResult(errors.New("unexpected error")))
 			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.Error(t, err)
-			},
 		},
 		{
-			name: "fail on invalid number of affected row",
-			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "failure_returns_invalid_number_of_affected_row",
+			arg:         c,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `INSERT INTO categories (id,created_at,updated_at,name,status) VALUES ($1,$2,$3,$4,$5)`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.ID, c.CreatedAt, c.UpdatedAt, c.Name, c.Status).WillReturnResult(sqlmock.NewResult(1, 2))
-			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.Error(t, err)
 			},
 		},
 		{
 			name: "success",
 			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `INSERT INTO categories (id,created_at,updated_at,name,status) VALUES ($1,$2,$3,$4,$5)`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.ID, c.CreatedAt, c.UpdatedAt, c.Name, c.Status).WillReturnResult(sqlmock.NewResult(1, 1))
-			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.NoError(t, err)
 			},
 		},
 	}
 
-	for _, tc := range testCases {
+	for i := range testCases {
+		tc := testCases[i]
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			db, mock, err := sqlmock.New()
 			assert.NoError(t, err)
 			repo := pg.NewCategoryRepository(db)
-			tc.builtSts(mock)
+			tc.prepare(mock)
 			err = repo.Store(context.TODO(), tc.arg)
-			tc.checkResponse(t, err)
+			if tc.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
 
 func Test_CategoryRepo_FindByID(t *testing.T) {
 	id := uuid.NewV4().String()
-	c := getCategory()
+	c := sample.NewCategory()
 	testCases := []struct {
-		name          string
-		arg           string
-		builtSts      func(mock sqlmock.Sqlmock)
-		checkResponse func(t *testing.T, res *domain.Category, err error)
+		name        string
+		arg         string
+		expectedErr bool
+		prepare     func(mock sqlmock.Sqlmock)
 	}{
 		{
-			name: "should fail if exec query returns err",
-			arg:  id,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "failure_exec_query_returns_error",
+			arg:         id,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `SELECT * FROM categories WHERE id = $1 ORDER BY id LIMIT 1`
 				mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(id).WillReturnError(errors.New("unexpected error"))
 			},
-			checkResponse: func(t *testing.T, res *domain.Category, err error) {
-				assert.Error(t, err)
-				assert.Nil(t, res)
-			},
 		},
 		{
-			name: "should succeed",
+			name: "success",
 			arg:  id,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			prepare: func(mock sqlmock.Sqlmock) {
 				row := sqlmock.
 					NewRows([]string{"id", "created_at", "updated_at", "name", "status"}).
 					AddRow(c.ID, c.CreatedAt, c.UpdatedAt, c.Name, c.Status)
 				query := `SELECT * FROM categories WHERE id = $1 ORDER BY id LIMIT 1`
 				mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(id).WillReturnRows(row)
 			},
-			checkResponse: func(t *testing.T, res *domain.Category, err error) {
-				assert.NoError(t, err)
-				assert.NotNil(t, res)
-				assert.Equal(t, res, c)
-			},
 		},
 	}
 
-	for _, tc := range testCases {
+	for i := range testCases {
+		tc := testCases[i]
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			db, mock, err := sqlmock.New()
 			assert.NoError(t, err)
 			repo := pg.NewCategoryRepository(db)
-			tc.builtSts(mock)
+			tc.prepare(mock)
 			res, err := repo.FindByID(context.TODO(), tc.arg)
-			tc.checkResponse(t, res, err)
+			if tc.expectedErr {
+				assert.Error(t, err)
+				assert.Nil(t, res)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+				assert.Equal(t, res, c)
+			}
 		})
 	}
 }
 
 func Test_CategoryRepo_Update(t *testing.T) {
-	c := getCategory()
+	c := sample.NewCategory()
 	testCases := []struct {
-		name          string
-		arg           *domain.Category
-		builtSts      func(mock sqlmock.Sqlmock)
-		checkResponse func(t *testing.T, err error)
+		name        string
+		arg         *domain.Category
+		expectedErr bool
+		prepare     func(mock sqlmock.Sqlmock)
 	}{
 		{
-			name: "fail on exec query",
-			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "failure_exec_query_returns_error",
+			arg:         c,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `UPDATE categories SET created_at=$1, updated_at=$2, name=$3, status=$4 WHERE id = $5`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.CreatedAt, c.UpdatedAt, c.Name, c.Status, c.ID).WillReturnError(errors.New("unexpected error"))
 			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.Error(t, err)
-			},
 		},
 		{
-			name: "fail on get affected row",
-			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "failure_get_affected_row_returns_error",
+			arg:         c,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `UPDATE categories SET created_at=$1, updated_at=$2, name=$3, status=$4 WHERE id = $5`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.CreatedAt, c.UpdatedAt, c.Name, c.Status, c.ID).WillReturnResult(sqlmock.NewErrorResult(errors.New("unexpected error")))
 			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.Error(t, err)
-			},
 		},
 		{
-			name: "fail on invalid number of affected row",
-			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			name:        "failure_returns_invalid_number_of_affected_row",
+			arg:         c,
+			expectedErr: true,
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `UPDATE categories SET created_at=$1, updated_at=$2, name=$3, status=$4 WHERE id = $5`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.CreatedAt, c.UpdatedAt, c.Name, c.Status, c.ID).WillReturnResult(sqlmock.NewResult(1, 2))
-			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.Error(t, err)
 			},
 		},
 		{
 			name: "success",
 			arg:  c,
-			builtSts: func(mock sqlmock.Sqlmock) {
+			prepare: func(mock sqlmock.Sqlmock) {
 				query := `UPDATE categories SET created_at=$1, updated_at=$2, name=$3, status=$4 WHERE id = $5`
 				mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(c.CreatedAt, c.UpdatedAt, c.Name, c.Status, c.ID).WillReturnResult(sqlmock.NewResult(1, 1))
-			},
-			checkResponse: func(t *testing.T, err error) {
-				assert.NoError(t, err)
 			},
 		},
 	}
 
-	for _, tc := range testCases {
+	for i := range testCases {
+		tc := testCases[i]
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			db, mock, err := sqlmock.New()
 			assert.NoError(t, err)
 			repo := pg.NewCategoryRepository(db)
-			tc.builtSts(mock)
+			tc.prepare(mock)
 			err = repo.Update(context.TODO(), tc.arg)
-			tc.checkResponse(t, err)
+			if tc.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
