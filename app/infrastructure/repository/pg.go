c161a9ded8fba75d3704d9e9456ab06386152e56
Edlâneo Júlio <edlanioj@gmail.com>
2021-08-04 16:03:41 +0000

    Update sql connection config

--- app/infrastructure/repository/pg.go
+++ app/infrastructure/repository/pg.go
@@ -3,11 +3,20 @@ package repository
 import (
 	"database/sql"
 
+	"github.com/EdlanioJ/kbu-store/app/config"
 	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func SqlConnection(dns string) *sql.DB {
-	db, err := sql.Open("postgres", dns)
+func SqlConnection(cfg *config.Config) *sql.DB {
+	var db *sql.DB
+	var err error
+
+	if cfg.Env != "test" {
+		db, err = sql.Open("postgres", cfg.Dns)
+	} else {
+		db, err = sql.Open("sqlite", cfg.DnsTest)
+	}
 	if err != nil {
 		panic(err)
 	}
